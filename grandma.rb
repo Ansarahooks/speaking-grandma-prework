be8e9ee1e6c2e5c8c1e8efdf45145a5e0e9db10f
ansara.hooks <ansara.hooks@gmail.com>
2019-02-14 17:59:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,9 +18,9 @@ if phrase == phrase.upcase
   "NO, NOT SINCE 1938!"
 
 elsif phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  "I LOVE YOU TOO PUMPKIN!"
 
 else
-  puts "HUH?! SPEAK UP, SONNY!"
+  "HUH?! SPEAK UP, SONNY!"
 end
 end
